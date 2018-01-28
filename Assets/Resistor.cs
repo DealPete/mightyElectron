3380809c168014325986a022c1bdc5e70f1ffebd
Peter Deal <pdeal@nl.rogers.com>
2018-01-27 20:47:37 -0330

    add hookupResistor and make wires move dynamically with nodes

--- Assets/Resistor.cs
+++ Assets/Resistor.cs
@@ -3,16 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Resistor : Wire {
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-	public override void DoAction(Agent agent){
+
+	public override void DoAction(Agent agent) {
 		agent.damage (1);
 	}
 }
