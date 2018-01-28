f1c21b5d5e54675840c1538dd97c96eb6a3e1e78
Peter Deal <pdeal@nl.rogers.com>
2018-01-27 23:36:07 -0330

    add some game logic

--- Assets/LED.cs
+++ Assets/LED.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LED : Wire {
+	public static int activeCount = 0;
 	public bool isOn = false;
 
 	public override void DoAction(Agent agent){
 		agent.damage (1);
-		isOn = true;
+		if (!isOn) {
+			isOn = true;
+			LED.activeCount += 1;
+		}
 	}
 }
---
+++ Assets/SignScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SignScript : MonoBehaviour {
+	public int energy;
+	public bool levelDone = false;
+	
+	// Update is called once per frame
+	void Update () {
+		string text;
+		if (levelDone) {
+			text = "LEVEL COMPLETE!!";
+		} else {
+			text = "Energy: " + energy;
+		}
+			
+		base.GetComponent<Text>().text = text;
+	}
+}
