a2a333e09ad19b1a1a8df8deefc1a59333b165cf
Peter Deal <pdeal@nl.rogers.com>
2018-02-08 14:28:35 -0330

    add crude title screen

---
+++ Assets/Scripts/UIScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIScript : MonoBehaviour {
+	public void Quit() {
+		Application.Quit();
+	}
+
+	public void LoadByIndex(int sceneIndex) {
+		SceneManager.LoadScene(sceneIndex);
+	}
+}
